b36e51b6283171b100486cd7578d4c59df1873cc
Judy_Faria <judy.faria.santos@gmail.com>
2022-11-22 13:39:34 -0300

    Aplicação de uma Calculadora usando POO com C#

---
+++ Documents/ByteBankADM/DesafioMatematica/Operacoes.cs
@@ -0,0 +1,26 @@
+﻿using Desafio_Matematica.Basicas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_Matematica
+{
+    public class Operacoes
+    {
+        public Soma Soma { get; set; }
+        public Subtracao Subtracao { get; set; }
+        public Multiplicacao Multiplicacao { get; set; }
+        public Divisao Divisao { get; set; }
+
+        public Operacoes()
+        {
+            this.Soma = new Soma();
+            this.Subtracao = new Subtracao();
+            this.Multiplicacao = new Multiplicacao();
+            this.Divisao = new Divisao();
+        }
+    }
+}
+
