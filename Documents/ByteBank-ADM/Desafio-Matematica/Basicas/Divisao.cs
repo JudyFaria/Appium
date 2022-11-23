a9d1b6a9649297511ff33d4fd43571ffe0ae804d
Judy_Faria <judy.faria.santos@gmail.com>
2022-11-23 13:40:12 -0300

    Commit depois de erro Desafio Matematica

---
+++ Documents/ByteBank-ADM/Desafio-Matematica/Basicas/Divisao.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_Matematica.Basicas
+{
+    public class Divisao
+    {
+        public double Calcular(double dividendo, double divisor)
+        {
+            if (divisor == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return dividendo / divisor;
+            }
+        }
+
+
+    }
+}
---
+++ Documents/ByteBank-ADM/Desafio-Matematica/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+using Desafio_Matematica;
+using System.ComponentModel;
+
+Console.WriteLine("Operaçoes Básica - Desafio Matemática");
+
+Operacoes operacoes = new Operacoes();
+
+Console.WriteLine("Soma: " + operacoes.Soma.Calcular(100,30));
+Console.WriteLine("Subitração: " + operacoes.Subtracao.Calcular(99, 3));
+Console.WriteLine("Multiplicação: " + operacoes.Multiplicacao.Calcular(55, 20));
+Console.WriteLine("Divisão: " + operacoes.Divisao.Calcular(63, 3));
